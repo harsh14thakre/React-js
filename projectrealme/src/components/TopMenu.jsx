19f7ffe93c886e6ee168886a86c739e5d6235fa9
harsh14thakre <harsh14thakre@gmail.com>
2025-05-12 12:43:51 +0530

    commit

--- projectrealme/src/components/TopMenu.jsx
+++ projectrealme/src/components/TopMenu.jsx
@@ -5,13 +5,12 @@ import { Link } from 'react-router-dom';
 
 const TopMenu=()=>{
     return(
-        <>
-        
+        <>       
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">realmi</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="home" >Store</Nav.Link>
+            <Nav.Link as={Link} to="home" >Home</Nav.Link>
             <Nav.Link as={Link} to="newlaunch" >New Launch</Nav.Link>
             <Nav.Link as={Link} to="phone" >Phone</Nav.Link>
             <Nav.Link as={Link} to="audio" >Audio</Nav.Link>
