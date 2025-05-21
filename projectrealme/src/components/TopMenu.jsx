6e2980bf10986bdc7f9aecea79b726e96a12cc78
harsh14thakre <harsh14thakre@gmail.com>
2025-05-21 21:30:04 +0530

    commit

--- projectrealme/src/components/TopMenu.jsx
+++ projectrealme/src/components/TopMenu.jsx
@@ -2,8 +2,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import { FaCartPlus } from "react-icons/fa";
+
+import { FaShoppingCart } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const TopMenu=()=>{
+
+  const navigate = useNavigate();
+  const cartData= useSelector(state=>state.mycart.cart);
+  console.log(cartData);
+  const cartLength=cartData.length;
     return(
         <>       
         <Navbar bg="dark" data-bs-theme="dark">
@@ -11,18 +19,18 @@ const TopMenu=()=>{
           <Navbar.Brand href="#home">realmi</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="home" >Home</Nav.Link>
-            <Nav.Link as={Link} to="newlaunch" >New Launch</Nav.Link>
             <Nav.Link as={Link} to="phone" >Phone</Nav.Link>
-            <Nav.Link as={Link} to="audio" >Audio</Nav.Link>
             <Nav.Link as={Link} to="accessories" >Accessories</Nav.Link>
 
-            <Nav.Link as={Link} to="community" >community</Nav.Link>
-            <Nav.Link as={Link} to="support" >Support</Nav.Link>
             <Nav.Link as={Link} to="brand" >Brand</Nav.Link>
-            <Nav.Link as={Link} to="realmiui" >realmi UI</Nav.Link>
 
             <Nav.Link as={Link} to="search" >Search</Nav.Link>
-            <FaCartPlus style={{color: "white", fontSize: "25px", display: "flex", }} />
+
+            <div id="header">
+            {cartLength}
+          <FaShoppingCart  onClick={()=>{navigate("/mycart")}} style={{cursor:"pointer"}}/>
+          </div>
+        
             
           </Nav>
         </Container>
---
+++ projectrealme/src/config/BackendUrl.jsx
@@ -0,0 +1,5 @@
+
+
+const BackEndURL="http://localhost:3000/Mobile";
+
+export default BackEndURL;
