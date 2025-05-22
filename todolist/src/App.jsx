384466652722d168b002974a8474fd7ac9801ff7
harsh14thakre <harsh14thakre@gmail.com>
2025-05-22 22:02:53 +0530

    commit

---
+++ todolist/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const App=()=>{
+  
+  const [MyData,setMyData]=
+
+  return(
+    <>
+
+    <h1 style={{textAlign:"center"}}>Todolist</h1>
+    <div id="inp">
+    <input type="text" placeholder="Enter here" />
+    <select name="" id=""></select>
+      <option value=""></option>
+      <option value=""></option>
+      <option value=""></option>
+
+     </div>
+
+    </>
+
+  )
+}
+export default App;
